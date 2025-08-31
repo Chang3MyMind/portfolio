19dfbcf932305d1aee81ca2d75b4c22dc7a045fa
Matheus Mattos <matheusmattos.r@gmail.com>
2025-08-30 22:03:29 -0300

    feat: Adiciona efeito de digitação no Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
+import Typewriter from "typewriter-effect";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
 
 function Hero() {
+  const [typingComplete, setTypingComplete] = useState(false);
+
   return (
     <>
       <section id="hero" className="sections-container">
@@ -9,10 +13,27 @@ function Hero() {
           <p className="color-text text-center text-xs font-normal md:col-span-full md:row-span-1 md:row-start-1 md:mx-auto md:text-base lg:text-lg xl:text-xl">
             Hello, I'm Matheus Mattos. A passionate Front-End Developer.
           </p>
-          <h1 className="color-text text-center text-lg font-semibold md:col-span-full md:row-span-1 md:row-start-2 md:mx-auto md:text-3xl lg:text-4xl xl:text-5xl">
-            &lt;Crafting intuitive
-            <span className="gradient-on-text"> Front-End </span>
-            experiences./&gt;
+          <h1 className="flex items-center justify-center gap-x-2 color-text text-center text-lg font-semibold md:col-span-full md:row-span-1 md:row-start-2 md:mx-auto md:text-3xl lg:text-4xl xl:text-5xl">
+            &lt;
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(
+                    'Crafting intuitive <span class="gradient-on-text"> Front-End </span> experiences.'
+                  )
+                  .pauseFor(1000)
+                  .callFunction(() => {
+                    setTypingComplete(true);
+                  })
+                  .start();
+              }}
+              options={{
+                delay: 50,
+                wrapperClassName: "color-text",
+                dangerouslyAllowHTML: true,
+              }}
+            />
+            {typingComplete && "/>"}
           </h1>
           <div className="row-start-3 flex gap-5 p-2 md:col-span-full md:row-span-1 md:mx-auto lg:gap-10">
             <button
