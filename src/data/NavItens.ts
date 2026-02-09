93b6f025655fcaf54039663ad54aa5b7ec0c661d
Matheus Mattos <matheusmattos.r@gmail.com>
2026-02-09 17:25:26 -0300

    Feat: Creating data for Navigation items.

---
+++ src/data/NavItens.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { label: "Ínicio", href: "#" },
+  { label: "Sobre", href: "#about" },
+  { label: "Habilidades", href: "#skills" },
+  { label: "Projetos", href: "#projects" },
+  { label: "Contato", href: "#contact" },
+];
